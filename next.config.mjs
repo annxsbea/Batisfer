f5f9bd321cc415ea3ce68e5d58600695b572f870
Anna Soares <125574851+annxsbea@users.noreply.github.com>
2024-10-10 16:23:55 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.optimization.minimizer.forEach((minimizer) => {
+        minimizer.options.parallelism = 1; // Limita a 1 worker
+      });
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
